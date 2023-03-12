be4f548bc3ca872aabe00144ae9f3921509bacc6
iam-amitdev <38841669+iam-amitdev@users.noreply.github.com>
2023-03-12 20:37:15 +0530

    Update syntax

--- hello.java
+++ hello.java
@@ -1,2 +1,13 @@
-Class Hello;
-PSVM;
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int length = nums.length;
+        int res=0;
+        for(int i=0;i<length;i++){
+            if(nums[i] != val){
+                nums[res]=nums[i];
+                res++;
+            }
+        }
+        return res;
+    }
+}
